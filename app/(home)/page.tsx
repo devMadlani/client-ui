865b15c1d6a46d018eb643ed371bd25cfe6a7315
Dev Madlani <madlanidev@gmail.com>
2026-02-09 19:36:28 +0530

    add category tab

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,31 +1,51 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <section className="bg-white">
-      <div className="container flex items-center justify-between py-24 mx-auto">
-        <div>
-          <h1 className="text-7xl font-black font-sans">
-            Super Delicious Pizza in <br />
-            <span className="text-primary">Only 45 Minutes!</span>
-          </h1>
-          <p className="text-2xl mt-8 max-w-lg leading-snug">
-            Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
-          </p>
-          <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
-            Get your pizza now
-          </Button>
+    <>
+      <section className="bg-white">
+        <div className="container flex items-center justify-between py-24 mx-auto">
+          <div>
+            <h1 className="text-7xl font-black font-sans ">
+              Super Delicious Pizza in <br />
+              <span className="text-primary">Only 45 Minutes!</span>
+            </h1>
+            <p className="text-2xl mt-8 max-w-lg leading-snug">
+              Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
+            </p>
+            <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
+              Get your pizza now
+            </Button>
+          </div>
+          <div>
+            <Image
+              alt="pizza-main"
+              src={"/pizza-main.png"}
+              width={400}
+              height={400}
+            />
+          </div>
         </div>
-        <div>
-          <Image
-            alt="pizza-main"
-            src={"/pizza-main.png"}
-            width={400}
-            height={400}
-          />
+      </section>
+
+      <section>
+        <div className="container py-12 mx-auto">
+          <Tabs defaultValue="pizza" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="pizza" className="text-md">
+                Pizza
+              </TabsTrigger>
+              <TabsTrigger value="beverages" className="text-md">
+                Beverages
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="pizza">Pizza list</TabsContent>
+            <TabsContent value="beverages">Beverages list </TabsContent>
+          </Tabs>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
